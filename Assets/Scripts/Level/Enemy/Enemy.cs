405eb6eb04bce727c5116ad814cfc145ef3334a9
Роберт Шевцов <91878034+RobertUAggo@users.noreply.github.com>
2023-05-17 04:43:39 +0300

    Update Enemy.cs

--- Assets/Scripts/Level/Enemy/Enemy.cs
+++ Assets/Scripts/Level/Enemy/Enemy.cs
@@ -1,11 +1,18 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 
 public class Enemy : BaseCreature
 {
+    [SerializeField] private float attackDistance = 2f;
+    [SerializeField] private float attackRate = 1f;
+    [SerializeField] private UnityEvent<Transform> Attack;
     private NavMeshAgent _navAgent;
+    private float _attackDistanceSqr;
     private void Awake()
     {
+        _attackDistanceSqr = attackDistance * attackDistance;
         _navAgent = GetComponent<NavMeshAgent>();
         OnTakeDamage.AddListener(AfterTakeDamage);
         OnDie.AddListener(AfterDie);
@@ -20,8 +27,31 @@ public class Enemy : BaseCreature
     {
 
     }
-    public void StartChase(Player player)
+    public void StartChase(Transform target)
     {
-        _navAgent.SetDestination(player.transform.position);
+        StopAllCoroutines();
+        StartCoroutine(C_ChaseLogic(target));
+        StartCoroutine(C_AttackLogic(target));
+    }
+    private IEnumerator C_ChaseLogic(Transform target)
+    {
+        yield return null;
+        while (IsAlive && target != null)
+        {
+            _navAgent.SetDestination(target.position);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+    private IEnumerator C_AttackLogic(Transform target)
+    {
+        yield return null;
+        while (IsAlive && target != null)
+        {
+            if ((transform.position - target.position).sqrMagnitude < _attackDistanceSqr)
+            {
+                Attack.Invoke(target);
+                yield return new WaitForSeconds(attackRate);
+            }
+        }
     }
 }
