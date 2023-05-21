cb9d4998c5134c47bed2419d7c380533ffa306a1
Роберт Шевцов <91878034+RobertUAggo@users.noreply.github.com>
2023-05-21 14:47:32 +0300

    Update LevelSaveData.cs

--- Assets/Scripts/Data/Level/LevelSaveData.cs
+++ Assets/Scripts/Data/Level/LevelSaveData.cs
@@ -2,5 +2,6 @@ using System.Collections.Generic;
 
 public class LevelSaveData
 {
+    public string Name = "Level name";
     public List<EnvironmentObjectSaveData> EnvironmentObjects = new List<EnvironmentObjectSaveData>();
 }
