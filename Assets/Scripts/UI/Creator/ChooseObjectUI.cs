cb53c1dee682f3fe35fc57fe7683cd85223df253
Роберт Шевцов <91878034+RobertUAggo@users.noreply.github.com>
2023-05-23 21:41:44 +0300

    More Creator

---
+++ Assets/Scripts/UI/Creator/ChooseObjectUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseObjectUI : MonoBehaviour
+{
+    [SerializeField] private Transform root;
+    private void Awake()
+    {
+        
+    }
+}
